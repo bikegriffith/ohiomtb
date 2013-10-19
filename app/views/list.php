4211326c65bbb211cadd2581ab844db49f50de68
techscap <techscap@a2ss4.a2hosting.com>
2013-10-19 03:26:05 -0400

    mobile compat

--- app/views/list.php
+++ app/views/list.php
@@ -14,11 +14,11 @@
 <div data-role="page" data-theme="a" id="demo-page" class="my-page">
 
 	<div data-role="header">
-		<h1>OhioMTB Trail Conditions<?=$tab=='recent' ? ': Recent Updates' : ''?></h1>
+		<h1 style="text-overflow:inherit;margin-right:auto;margin-left:auto;">OhioMTB <?=$tab=='recent' ? ' Newest Trail Updates' : 'Trail Conditions'?></h1>
 	</div><!-- /header -->
 	
-	<div data-role="content">
-	<iframe src="//www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.facebook.com%2Fohiomtb&amp;width=200&amp;height=50&amp;colorscheme=dark&amp;layout=standard&amp;action=like&amp;show_faces=false&amp;send=true" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:200px; height:50px; margin:15px 10px 0 0; float:right;" allowTransparency="true"></iframe>	
+	<div data-role="content" style="padding-left:5px;padding-right:5px;">
+	<iframe src="//www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.facebook.com%2Fohiomtb&amp;width=200&amp;height=50&amp;colorscheme=dark&amp;layout=standard&amp;action=like&amp;show_faces=false&amp;send=true" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:200px; height:50px; position:absolute;top:10px; right:15px;" id="facebooklikeframe" allowTransparency="true"></iframe>	
 
 	<?php if (Session::get('message')) { ?>
 	<br style="clear:both;">
@@ -27,15 +27,15 @@
 
         <ul data-role="listview" data-inset="true" data-filter="true" data-filter-placeholder="Search for trails...">
 	<?php foreach($trails as $trail) { ?>
-        	<li><a href="/trails/<?=$trail->slug?>">
+        	<li><a style="padding-left:90px" href="/trails/<?=$trail->slug?>">
 		<?php if ($trail->modifieddate > date('Y-m-d H:i:s', strtotime('-4 WEEKS'))) { ?>
             	<img alt="Trail conditions are <?=ViewUtils::statusToText($trail->status)?>" src="/img/statuses/<?=$trail->status?>.png">
-            	<h2><?=$trail->name?></h2>
+            	<h2 style="overflow:visible"><?=$trail->name?></h2>
                 <p><?=htmlspecialchars(ViewUtils::truncate_if_necessary($trail->conditions, 40))?></p>
                 <p class="ui-li-aside"><?=ViewUtils::time_since($trail->modifieddate)?> ago</p>
 		<?php } else { ?>
             	<img alt="Status is too old, needs an update" src="/img/statuses/old.png">
-            	<h2><?=$trail->name?></h2>
+            	<h2 style="overflow:visible"><?=$trail->name?></h2>
 		<p>&nbsp;</p>
                 <p class="ui-li-aside" style="color:#b00"><?=ViewUtils::time_since($trail->modifieddate)?> ago</p>
 		<?php } ?>
@@ -46,13 +46,13 @@
     </div><!-- /content -->
     
     <div data-role="footer" data-position="fixed">
-	<div data-role="navbar" data-iconpos="left">
+	<div data-role="navbar" data-iconpos="top">
 		<ul>
 			<li><a href="/" data-icon="grid" <?=$tab=='all' ? 'class="ui-btn-active"' : ''?>>All Trails</a></li>
-			<li><a href="/recent-updates" data-icon="gear" <?=$tab=='recent' ? 'class="ui-btn-active"' : ''?>>Recently Updated</a></li>
+			<li><a href="/recent-updates" data-icon="gear" <?=$tab=='recent' ? 'class="ui-btn-active"' : ''?>>Newest Updates</a></li>
 			<!--<li><a href="javascript:alert('Coming Soon!')" data-icon="star">My Favorites</a></li>-->
-			<li><a href="/about" data-icon="info">About OhioMTB</a></li>
 			<!--<li><a href="#" data-icon="gear">Close to Me</a></li>-->
+			<li><a href="/about" data-icon="info">About</a></li>
 		</ul>
 	</div>
     </div><!-- /footer -->
