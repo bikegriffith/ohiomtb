9b42ff629de4b0df367670c62d8870bd4b95af94
Mike Griffith <bikegriffith@gmail.com>
2015-10-23 21:38:43 -0400

    render list of trail slugs

---
+++ src/components/facebook/likebox.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export default class FacebookLikeBox extends React.Component {
+  render() {
+    let style = {
+      border:'none',
+      overflow:'hidden',
+      width:'200px',
+      height:'50px',
+      position:'absolute',
+      top:'10px',
+      right:'15px'
+    };
+    return (
+      <iframe src="//www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.facebook.com%2Fohiomtb&amp;width=200&amp;height=50&amp;colorscheme=dark&amp;layout=standard&amp;action=like&amp;show_faces=false&amp;send=true"
+              scrolling="no"
+              frameborder="0"
+              style={style}
+              id="facebooklikeframe"
+              allowTransparency="true"></iframe>  
+    )
+  }
+}
--- src/pages/home/page.jsx
+++ src/pages/home/page.jsx
@@ -1,17 +1,25 @@
 import React from "react";
 import { getData } from "../../common/request";
 import styles from "./style.css";
-
+import FacebookLikeBox from "../../components/facebook/likebox.js";
 import data from "../../data";
 
 export default class HomePage extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+        trails: []
+    };
+  }
+
   componentWillMount() {
-    console.log("[HomePage] will mount with server response: ", this.props);
     data.getTrails().then(this.handleTrailsSuccess.bind(this), this.handleTrailsError.bind(this));
   }
 
   handleTrailsSuccess(result) {
-    console.log('found trails', result.map((r) => r.get('slug')));
+    let trails = result.map((r) => { return {slug: r.get('slug'), name: r.get('name')} });
+    this.setState({ trails: trails });
+    console.log('found trails', this.state.trails);
   }
 
   handleTrailsError(error) {
@@ -19,13 +27,23 @@ export default class HomePage extends React.Component {
   }
 
   render() {
-    let { title } = 'home'; //this.props.data.home;
+    let { title } = 'OhioMTB Trail Conditions';
 
     return (
       <div className={styles.content}>
         <h1>{title}</h1>
-        <p className={styles.welcomeText}>Thanks for joining!</p>
+        <FacebookLikeBox />
+        <p className={styles.welcomeText}>All Trails:</p>
+        <ul>
+            {this.state.trails.map((t) => this.renderTrail(t))}
+        </ul>
       </div>
+    );
+  }
+
+  renderTrail(trail) {
+    return (
+        <li>{trail.slug}</li>
     );
   }
 
